2983ebfec992b671e55e0d7155e4798adfe2dc81
Brien Smith Martínez <bsmartinez@gmail.com>
2019-08-02 19:02:03 +0200

    Rename webapi project and add to solution

--- AEMETData.WebApi/Program.cs
+++ AEMETData.WebApi/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace AEMETData
+namespace AEMETData.WebApi
 {
     public class Program
     {
